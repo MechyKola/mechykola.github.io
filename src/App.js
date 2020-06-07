6de703c6769ac4d57e4773ad73c2324a932da126
MechyKola <mv.nechyporuk@gmail.com>
2020-06-07 17:09:05 +0100

    added a bit material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Divider } from '@material-ui/core'
+import { Divider, Grid, Box } from '@material-ui/core'
 import './App.css';
+import avatar from './avatar.jpg';
 
 function App() {
   return (
@@ -12,13 +13,29 @@ function App() {
         <p className="Quote">"Code is kinda cool but I wouldn't do it as a job"</p>
         <p className="Author"> - me, before becoming a software engineer</p>
       </header>
-      <h1>
-        About me
-      </h1>
-      <Divider />
-      <p>
-        I'm currently a computer science student at the University of Bristol.
-      </p>
+      <content>
+        <Grid container justify="space-evenly" className="About-item">
+          <Grid item xs={8} lg={4} className="About-item">
+            <img src={avatar} alt="Avatar" className="Avatar" />
+          </Grid>
+          <Grid item xs={12} lg={8} className="About-item">
+            <h1>
+              About me
+            </h1>
+            <Divider />
+            <p>
+              I'm currently study computer science at the University of Bristol.
+              Outside my studies I am the president of The CSS (Computer Science Society)
+              of the University of Bristol, and take part in the Russian speaking society.
+            </p>
+          </Grid>
+        </Grid>
+        <Box s={12}>
+          <p>
+            Something about what I've done I guess
+          </p>
+        </Box>
+      </content>
     </div>
   );
 }
